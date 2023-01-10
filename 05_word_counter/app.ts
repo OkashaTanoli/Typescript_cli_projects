a3b2a073e7be63be4fe7f1aa26d391e9e3cc3b1b
OkashaTanoli <okashatanoli12345@gmail.com>
2023-01-11 03:14:05 +0500

    word counter

---
+++ 05_word_counter/app.ts
@@ -0,0 +1,48 @@
+// #!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import { createSpinner } from 'nanospinner';
+
+let sleep = () => new Promise((r) => setTimeout(r, 2000))
+
+async function ParaInput() {
+    const input = await inquirer.prompt([{
+        name: 'input',
+        message: chalk.rgb(249, 140, 255)('Enter Paragraph'),
+    }])
+    let value: string = await input['input']
+    return value
+}
+
+function WordsAndLettersCounter(para: string) {
+    let words = para.split(' ').filter((val)=>val !== '')
+    let letters = words.join('')
+    return { words: words.length, letters: letters.length }
+}
+
+
+// Program Entry Point
+
+while (true) {
+    const para = await ParaInput()
+    const { words, letters } = WordsAndLettersCounter(para)
+    //Spinner
+    const spinner = createSpinner('Counting').start()
+    await sleep()
+    spinner.success()
+
+    console.log(chalk.bgWhiteBright.blackBright.bold(`   Total Words : ${words}  `))
+    console.log(chalk.bgWhiteBright.blackBright.bold(` Total Letters : ${letters}  `))
+
+    const input = await inquirer.prompt([
+        {
+            name: chalk.rgb(255, 255, 160)(`Do You Want To Exit?`),
+            type: "confirm",
+        }
+    ])
+    let value: boolean = await input['\x1B[38;2;255;255;160mDo You Want To Exit?\x1B[39m']
+    if (value) {
+        break;
+    }
+}
